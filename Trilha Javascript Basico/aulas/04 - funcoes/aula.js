8e741cae2a6b4b752b9f8d1cfd6655742fa27fcb
João César dos Santos Quirino <jaocesarsantos@gmail.com>
2022-11-28 20:43:08 -0300

    Function

---
+++ Trilha Javascript Basico/aulas/04 - funcoes/aula.js
@@ -0,0 +1,38 @@
+/*
+function calcularJuros() {
+
+}
+
+function main() {
+    console.log('Programa principal');
+    calcularJuros();
+}
+
+main();
+*/
+
+function calcularImc(peso, altura) {
+    return peso / Math.pow(altura, 2);
+}
+
+function classificarImc(imc) {
+    if (imc < 18.5){
+        return('Abaixo do peso');
+    } else if (imc >= 18.5 && imc < 25){
+        return('Peso normal');
+    } else if (imc >= 25 && imc < 30){
+        return('Acima do peso');
+    } else if (imc >= 30 && imc < 40){
+        return('Obeso');
+    } else {
+        return('Obesidade Grave');
+    }
+}
+
+(function () {
+    const peso = 79;
+    const altura = 1.86;
+
+    let imc = calcularImc(peso, altura);
+    console.log(classificarImc(imc));
+})();
